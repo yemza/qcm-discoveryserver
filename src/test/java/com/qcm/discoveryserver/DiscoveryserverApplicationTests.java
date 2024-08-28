f897d3c01b7013d6a64c447829703445ec4c16ba
El Bouazzaoui <hfelboua@amazon.com>
2024-08-28 09:42:07 +0100

    first commit

---
+++ src/test/java/com/qcm/discoveryserver/DiscoveryserverApplicationTests.java
@@ -0,0 +1,13 @@
+package com.qcm.discoveryserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscoveryserverApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
